fc9539209954007910920626f059fe515c9093be
peyton.morrison <peyton.morrison@utiliflex.com>
2023-12-06 15:27:47 -0500

    video 03-transforms
    Used elixir pipes to change data
    
    Signed-off-by: peyton.morrison <peyton.morrison@utiliflex.com>

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -1,6 +1,37 @@
 defmodule Servy.Handler do
   def handle(request) do
+    # conv = parse(request)
+    # conv = route(conv)
+    # format_response(conv)
+
+    # Use pipes insted
+
+    request
+    |> parse
+    |> route
+    |> format_response
   end
+  def parse(request) do
+    # TODO: Parse the request string into a map:
+    conv = %{ method: "GET", path: "/wildthings", resp_body: "" }
+  end
+
+  def route(conv) do
+    # TODO: Create a new map that also has the response body:
+    conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
+  end
+
+  def format_response(conv) do
+    # TODO: Use values in the map to create an HTTP response string:
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: 20
+
+    Bears, Lions, Tigers
+    """
+  end
+
 end
 
 request = """
@@ -9,14 +40,6 @@ Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
 
-"""
-
-expected_response = """
-HTTP/1.1 OK
-Content-Type: text/html
-Content-Length: 20
-
-Bears, Lions, Tigers
 """
 
 response = Servy.Handler.handle(request)
