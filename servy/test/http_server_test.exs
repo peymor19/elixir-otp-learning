95058ae7fb3d12453984c3f9b531a1cc002b64a4
peyton.morrison <peyton.morrison@utiliflex.com>
2024-01-15 08:51:12 -0500

    fixing tests
    Signed-off-by: peyton.morrison <peyton.morrison@utiliflex.com>

--- servy/test/http_server_test.exs
+++ servy/test/http_server_test.exs
@@ -39,16 +39,31 @@ defmodule HttpServerTest do
     assert response == assert_response
   end
 
-  test "accecpt 5 requests on a socket and sends back a response using HTTPoison" do
-
+  test "accepts 5 concurrent requests on a socket and sends back a response" do
     spawn(HttpServer, :start, [4000])
 
     parent = self()
 
     max_concurrent_requests = 5
 
-    1..max_concurrent_requests |> Enum.each(fn _ -> spawn(fn -> send(parent, {:ok, HTTPoison.get("http://localhost:4000/wildthings")} ) end) end)
+    # Spawn the client processes
+    for _ <- 1..max_concurrent_requests do
+        spawn(fn ->
+          # Send the request
+          {:ok, response} = HTTPoison.get "http://localhost:4000/wildthings"
 
-    1..max_concurrent_requests |> Enum.each(fn _ -> receive do {:ok, response} -> assert response.status_code == 200 end end)
+          # Send the response back to the parent
+          send(parent, {:ok, response})
+        end)
+      end
+
+    # Await all {:handled, response} messages from spawned processes.
+    for _ <- 1..max_concurrent_requests do
+      receive do
+        {:ok, response} ->
+          assert response.status_code == 200
+          assert response.body == "Bears, Lions, Tigers"
+      end
+    end
   end
 end
