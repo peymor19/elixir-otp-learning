c93815a93fa7fe508f6053b1b5e4c95397c33c9f
peymor19 <peyton9148@gmail.com>
2024-02-21 01:25:06 -0500

    build a generic server
    Signed-off-by: peymor19 <peyton9148@gmail.com>

--- servy/lib/servy/counter.ex
+++ servy/lib/servy/counter.ex
@@ -1,45 +1,104 @@
+defmodule Servy.GenericServer do
+
+  def start(callback_module, initial_state, name) do
+    pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
+    Process.register(pid, name)
+    pid
+  end
+
+  def call(pid, message) do
+    send pid, {:call, self(), message}
+
+    receive do {:response, response} -> response end
+  end
+
+  def cast(pid, message) do
+    send pid, {:cast, message}
+  end
+
+  def listen_loop(state, callback_module) do
+    receive do
+      {:call, sender, message} when is_pid(sender) ->
+        {response, new_state} = callback_module.handle_call(message, state)
+        send sender, {:response, response}
+        listen_loop(new_state, callback_module)
+      {:cast, message} ->
+        new_state = callback_module.handle_cast(message, state)
+        listen_loop(new_state, callback_module)
+      unexpected ->
+        IO.puts "Unexpected message: #{inspect unexpected}"
+        listen_loop(state, callback_module)
+    end
+  end
+end
+
 defmodule Servy.Counter do
 
   @name :counter
 
+  alias Servy.GenericServer
+
   def start() do
-    pid = spawn(__MODULE__, :listen_loop, [%{}])
-    Process.register(pid, @name)
-    pid
+    Servy.GenericServer.start(__MODULE__, %{}, @name)
   end
 
   def bump_count(url) do
-    send @name, {self(), :bump_count, url}
-
-    receive do {:response, status} -> status end
+    GenericServer.call @name, {:bump_count, url}
   end
 
   def get_count(url) do
-    send @name, {self(), :get_count, url}
-    receive do {:response, status} -> status end
+    GenericServer.call @name, {:get_count, url}
   end
 
   def get_counts() do
-    send @name, {self(), :get_counts}
-    receive do {:response, status} -> status end
+    GenericServer.call @name, :get_counts
   end
 
-  def listen_loop(state) do
-    receive do
-      {sender, :bump_count, url} ->
-        new_state = Map.update(state, url, 1, &(&1 + 1))
-        send sender, {:response, Map.get(new_state, url)}
-        listen_loop(new_state)
-      {sender, :get_count, url} ->
-        count = Map.get(state, url, 0)
-        send sender, {:response, count}
-        listen_loop(state)
-      {sender, :get_counts} ->
-        send sender, {:response, state}
-        listen_loop(state)
-      unexpected ->
-        IO.puts "Unexpected message: #{inspect unexpected}"
-        listen_loop(state)
-    end
+  def reset() do
+    GenericServer.cast @name, :reset
+  end
+
+  def handle_call({:bump_count, url}, state) do
+      new_state = Map.update(state, url, 1, &(&1 + 1))
+      {:ok, new_state}
+  end
+
+  def handle_call({:get_count, url}, state) do
+      count = Map.get(state, url, 0)
+      {count, state}
+  end
+
+  def handle_call(:get_counts, state) do
+      {state, state}
+  end
+
+  def handle_cast(:reset, _state) do
+      %{}
   end
 end
+
+
+alias Servy.Counter
+
+Counter.start()
+
+IO.inspect Counter.bump_count("/hello")
+IO.inspect Counter.bump_count("/hello")
+IO.inspect Counter.bump_count("/hello")
+IO.inspect Counter.bump_count("/hello")
+
+IO.inspect Counter.get_counts()
+
+IO.inspect Counter.reset()
+
+IO.inspect Counter.get_counts()
+
+IO.inspect Counter.bump_count("/hello")
+IO.inspect Counter.bump_count("/hello")
+IO.inspect Counter.bump_count("/hello")
+IO.inspect Counter.bump_count("/hello")
+
+IO.inspect Counter.get_counts()
+
+
+
